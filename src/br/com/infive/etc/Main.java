64698ba2126e4d0fc64f9c09240e3fa532083961
Rafael Mardegan <Rafa@OPTIMUS>
2020-02-26 20:51:34 -0300

    > refactor main

--- src/br/com/infive/etc/Main.java
+++ src/br/com/infive/etc/Main.java
@@ -4,12 +4,11 @@ import java.util.Random;
 
 public class Main {
 
-	//Intellij IDEA 14 License Key
 	@SuppressWarnings("unused")
 	public static void main(String[] args)
 	{
 		KeyGenerator keyGenerator = new KeyGenerator();
-		String name = "Gurkan CAKIR";
+		String name = "define a name";
 		
 		Random r = new Random();
 		System.out.println(KeyGenerator.MakeKey(name, 0, r.nextInt(100000)));
